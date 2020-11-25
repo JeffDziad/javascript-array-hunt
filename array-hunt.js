d189ad508fdff34452fdd3dfbcc5d088d714104f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-25 00:54:03 -0600

    Finished upperVowel and stringReverse.

--- array-hunt.js
+++ array-hunt.js
@@ -94,43 +94,150 @@ $(document).ready(function () {
         Output them to td#firstLast
          */
 
+        firstLast(myArray);
 
         /*
         Find the first string that contains an 'n'.
         Output it to td#firstEnn
          */
 
+        firstN(myArray);
 
         /*
         Find all of the strings with less than 6 characters.
         Output them to td#lessThanSix
-         */
+        */
 
+        lessThanSix(myArray);
 
         /*
         Find the longest string in the array.
         Output it to td#longName
          */
 
+        longest(myArray);
 
         /*
         Find all of the strings that do not contain the letter 's'.
         Output them to td#noEss
          */
 
+        noS(myArray);
 
         /*
         Output all of the strings, but with all of their vowels
         in uppercase, to td#upperVowels
          */
 
+        upperVowel(myArray);
 
         /*
         Output all of the strings in reverse order and separated by
         ' - ' to td#reverseDash
          */
+        stringReverse(myArray);
+    }
 
+    function firstLast(myArray)
+    {
+        var first = myArray[0];
+        var last = myArray[myArray.length - 1];
+        $("td#firstLast").text(first + ', ' + last);
+    }
 
+    function firstN(myArray)
+    {
+        for(var i = 0; i <= myArray.length; i++)
+        {
+            if(myArray[i].includes("n"))
+            {
+                $("td#firstEnn").text(myArray[i]);
+                break;
+            }
+        }
+    }
+
+    function lessThanSix(myArray)
+    {
+        for(var q = 0; q <= myArray.length; q++)
+        {
+            if(q === 0)
+            {
+                $("td#lessThanSix").text("");
+            }
+            if(myArray[q] && myArray[q].length < 6 )
+            {
+                $("td#lessThanSix").append(myArray[q] + ", ");
+            }
+        }
+    }
+
+    function longest(myArray)
+    {
+        var longest = myArray.reduce(
+            function (a, b)
+            {
+                return a.length > b.length ? a : b;
+            }
+        );
+        $("td#longName").text(longest);
+    }
+
+    function noS(myArray)
+    {
+        for(var x = 0; x <= myArray.length; x++)
+        {
+            if(x === 0)
+            {
+                $("td#noEss").text("");
+            }
+            if(myArray[x] && myArray[x].includes("s") === false)
+            {
+                $("td#noEss").append(myArray[x] + ", ");
+            }
+        }
+    }
+
+    function upperVowel(myArray)
+    {
+        var vowels = ["a", "e", "i", "o", "u"];
+        for(var i = 0; i < myArray.length; i++)
+        {
+            if(i === 0)
+            {
+                $("td#upperVowels").text("");
+            }
+            var first = myArray[i].split('');
+            for(var o = 0; o < first.length; o++)
+            {
+                var char = first[o];
+                for(var p = 0; p < vowels.length; p++)
+                {
+                    if(char === vowels[p])
+                    {
+                        var upper = char.toUpperCase();
+                        first[o] = upper;
+                    }
+                }
+            }
+            $("td#upperVowels").append(first.join("") + ", ");
+        }
+
+    }
+
+    function stringReverse(myArray)
+    {
+       for(var i = 0; i < myArray.length; i++)
+       {
+           if(i === 0)
+           {
+               $("td#reverseDash").text("");
+           }
+           var splitted = myArray[i].split("");
+           var reversed = splitted.reverse();
+           var joined = reversed.join("-");
+           $("td#reverseDash").append(joined + ", ");
+       }
     }
 
 });
